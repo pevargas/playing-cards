316c67100f7f8f50f479685728f0465a6f7de1d9
Patrick Vargas <patrick@yesthisispatrick.com>
2016-06-27 00:40:17 -0400

    feat(card): added a flip method to flip card
    this member tells us if the card is visible to players or not

--- src/main/card.js
+++ src/main/card.js
@@ -3,10 +3,15 @@
 var Card = function (rank, suit) {
   this.rank = rank
   this.suit = suit
+  this.isFaceUp = false
 }
 
 Card.prototype.toString = function () {
   return this.rank + ' of ' + this.suit
+}
+
+Card.prototype.flip = function () {
+  this.isFaceUp = !this.isFaceUp
 }
 
 module.exports = Card
--- src/test/card.test.js
+++ src/test/card.test.js
@@ -10,3 +10,15 @@ describe('toString', function () {
     expect(testCard.toString()).to.equal('Ace of Spade')
   })
 })
+
+describe('flip', function () {
+  it('should be upside-down when initlalized', function () {
+    var card = new Card('Ace', 'Spade')
+    expect(card.isFaceUp).to.be.false
+  })
+  it('should turn right-side-up when flipped', function () {
+    var card = new Card('Ace', 'Spade')
+    card.flip()
+    expect(card.isFaceUp).to.be.true
+  })
+})
