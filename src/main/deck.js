a916cd60a1584a1a79a5d12ed499920fcff0939b
Patrick Vargas <patrick@yesthisispatrick.com>
2016-06-27 00:41:22 -0400

    feat(deck): Flushed out deck class with more methods
    Added a draw and discard method, and used the DECK_TYPES for testing

--- src/main/deck.js
+++ src/main/deck.js
@@ -2,8 +2,39 @@
 
 var Card = require('./card.js')
 
-var Deck = function () {
-  this.drawPile = [new Card('Fuck', 'You')]
+var Deck = function (DECK_TYPE) {
+  if (!DECK_TYPE.hasOwnProperty('ranks') && !Array.isArray(DECK_TYPE.ranks)) {
+    throw new TypeError('Expected DECK_TYPE to contain a \'ranks\' array')
+  }
+  if (!DECK_TYPE.hasOwnProperty('suits') && !Array.isArray(DECK_TYPE.suits)) {
+    throw new TypeError('Expected DECK_TYPE to contain a \'suits\' array')
+  }
+  if (!DECK_TYPE.hasOwnProperty('jokers') || !Array.isArray(DECK_TYPE.jokers)) {
+    DECK_TYPE.jokers = []
+  }
+
+  this.drawPile = []
+  this.discardPile = []
+  for (var s = 0; s < DECK_TYPE.suits.length; ++s) {
+    for (var r = 0; r < DECK_TYPE.ranks.length; ++r) {
+      this.drawPile.push(new Card(DECK_TYPE.ranks[r], DECK_TYPE.suits[s]))
+    }
+  }
+
+  for (var joker in DECK_TYPE.jokers) {
+    this.drawPile.push(new Card(joker, joker))
+  }
+}
+
+Deck.prototype.draw = function () {
+  return this.drawPile.pop()
+}
+
+Deck.prototype.discard = function (card) {
+  if (!card instanceof Card) {
+    throw new TypeError('Expected \'card\' to be of type Card')
+  }
+  this.discardPile.push(card)
 }
 
 module.exports = Deck
--- src/test/deck.test.js
+++ src/test/deck.test.js
@@ -3,10 +3,65 @@
 var common = require('./common.js')
 var expect = common.expect
 var Deck = common.playingCards.Deck
+var Card = common.playingCards.Card
+var STANDARD = common.playingCards.DECK_TYPES.STANDARD
 
 describe('drawPile', function () {
-  it('should return a list of cards ', function () {
-    var testDeck = new Deck()
-    expect(testDeck.drawPile).to.have.length(1)
+  it('should contain a drawPile array', function () {
+    var deck = new Deck(STANDARD)
+    expect(deck).to.have.property('drawPile')
+      .and.to.be.an.instanceof(Array)
+  })
+
+  it('should contain as some cards', function () {
+    var deck = new Deck(STANDARD)
+    expect(deck).to.not.be.empty
+  })
+})
+
+describe('discardPile', function () {
+  it('should contain a discardPile array', function () {
+    var deck = new Deck(STANDARD)
+    expect(deck).to.have.property('discardPile')
+      .and.to.be.an.instanceof(Array)
+  })
+
+  it('should be empty when intialized', function () {
+    var deck = new Deck(STANDARD)
+    expect(deck.discardPile).to.be.empty
+  })
+})
+
+describe('draw', function () {
+  it('should give a card', function () {
+    var deck = new Deck(STANDARD)
+    var card = deck.draw()
+    expect(card).to.be.instanceof(Card)
+  })
+
+  it('should decrease the draw pile size', function () {
+    var deck = new Deck(STANDARD)
+    var numDraw = deck.drawPile.length
+    deck.draw()
+    expect(deck.drawPile).to.have.length.of(numDraw - 1)
+  })
+})
+
+describe('discard', function () {
+  it('should add a card to the discardPile', function () {
+    var deck = new Deck(STANDARD)
+    var numDiscard = deck.discardPile.length
+    var card = deck.draw()
+    deck.discard(card)
+    expect(deck.discardPile).to.have.length.of(numDiscard + 1)
+  })
+
+  it('should have the origainl numer of cards after one draw and one discard', function () {
+    var deck = new Deck(STANDARD)
+    var total = STANDARD.ranks.length * STANDARD.suits.length + STANDARD.jokers.length
+    var card = deck.draw()
+    deck.discard(card)
+    var newTotal = deck.discardPile.length + deck.drawPile.length
+    expect(newTotal).to.equal(total)
   })
 })
